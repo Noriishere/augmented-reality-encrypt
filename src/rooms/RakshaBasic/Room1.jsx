cd14aef1fa7c63c6fafc4745df007932687beba4
Noriishere <noriyamashin@gmail.com>
2026-07-10 22:04:40 +0700

    Feat: ini dia

--- src/rooms/RakshaBasic/Room1.jsx
+++ src/rooms/RakshaBasic/Room1.jsx
@@ -122,8 +122,8 @@ function AudioTerminal({ position, rotation = "0 0 0" }) {
    OCTAGON WALLS
    ============================================================ */
 function OctagonWalls() {
-    const RADIUS = 14;
-    const SIDE = 3 * RADIUS * Math.sin(Math.PI / 8); // panjang sisi octagon
+    const RADIUS = 12;
+    const SIDE = 2.5 * RADIUS * Math.sin(Math.PI / 8); // panjang sisi octagon
 
     const walls = Array.from({ length: 8 }, (_, i) => {
         const angleDeg = i * 45;
